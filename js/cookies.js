ebfd88b204a2e4efbc2fb7df7cfa48ca4e2769f4
dan <25mb@gaox.eu>
2026-09-01 23:25:35 +0200

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -41,10 +41,6 @@
        if (val==="1624cd338755118684697bb4b2f29117a8c246e345d02118815d8ae2b0ad28ff") {
          $("#master").show();
          $("#pwmaster").hide();
-         //renew cookie  
-         newval = sha2;
-         setCookie(myCookieName, newval, 1); 
-         return;
        }
     }, true);
  
